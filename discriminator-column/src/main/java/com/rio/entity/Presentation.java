beb1ba2d9191dfbd716a7b9501032837e9e4951c
Andriy Riy <andriy.riy.extern@unzer.com>
2023-06-14 21:19:36 +0300

    refactoring

--- discriminator-column/src/main/java/com/rio/entity/Presentation.java
+++ discriminator-column/src/main/java/com/rio/entity/Presentation.java
@@ -1,10 +1,15 @@
 package com.rio.entity;
 
+import com.rio.conf.TenantHolder;
+import io.micrometer.common.util.StringUtils;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreRemove;
+import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -34,4 +39,14 @@ public class Presentation {
   private String tenantId;
   @Column(name = "created_at")
   private LocalDateTime createdAt;
+
+  @PreUpdate
+  @PreRemove
+  @PrePersist
+  public void setTenant() {
+    String currentTenant = TenantHolder.getCurrentTenant();
+    if (StringUtils.isNotEmpty(currentTenant)) {
+      this.tenantId = currentTenant;
+    }
+  }
 }
